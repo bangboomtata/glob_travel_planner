fa311c3fdb2354a30ea20b72980779525ee6db15
bangboomtata <bongb7054@gmail.com>
2024-10-28 16:32:25 +0000

    started on user preference screen, added aceternity ui component

--- src/app/common.tsx
+++ src/app/common.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
-import { UserRound, ShoppingCart } from 'lucide-react'
+import { UserRound } from 'lucide-react'
 
 const navLink = [
    {
